8a08766b2ad97efacfe70616d8ba7375f5be71d0
RNSinha <rnsinha@sahajsolutions.com>
2024-08-22 13:25:58 +0530

    Your commit message

--- database.py
+++ database.py
@@ -1,6 +1,10 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 
-MONGO_DETAILS = "mongodb+srv://sahajinfodelhi:HTXumrFEX9KPHTF1@cluster0.hqy76hs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tls=true&tlsInsecure=true"
+# MONGO_DETAILS = "mongodb+srv://sahajinfodelhi:HTXumrFEX9KPHTF1@cluster0.hqy76hs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tls=true&tlsInsecure=true"
+
+
+MONGO_DETAILS = "mongodb+srv://sahajinfodelhi:HTXumrFEX9KPHTF1@cluster0.hqy76hs.mongodb.net/user_auth_db?retryWrites=true&w=majority&tls=true"
+
 
 client = AsyncIOMotorClient(MONGO_DETAILS)
 
